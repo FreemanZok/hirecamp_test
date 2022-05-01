a14b16cc256ba3a991653bc0a3907b8652349602
ali zokaei <ali.zokaei.1367@gmail.com>
2022-05-01 10:58:37 +0430

    hi helllo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="hello">
-  <div>
-    hello alizoka
-  </div>
+  <div class="">
+    <div class="text-center my-container px-4">
+      <h2 class="mb-4 py-2">
+        hello, this is a test...
+      </h2>
+      <div>
+        <v-select :items="myItems" label="Solo field" solo></v-select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-}
-</script>
+  name: "HelloWorld",
 
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  data: () => ({
+    myItems:['Foo', 'Bar', 'Fizz', 'Buzz'],
+  }),
+};
+</script>
+<style lang="scss" scoped>
+.my-container {
+  max-width: 800px;
+  margin: 0 auto;
+  background: rgb(200, 248, 252);
+  min-height: 100vh;
 }
 </style>
