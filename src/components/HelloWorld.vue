174721aa869f1dd2eac3edd76d27d9dbeea6818f
ali zokaei <ali.zokaei.1367@gmail.com>
2022-05-02 17:37:31 +0430

    hi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="">
-    <div class="text-center my-container px-4">
-      <h2 class="mb-4 py-2">hello, this is a test...</h2>
+  <div class="rtl">
+    <div class="text-center my-container px-4 py-4">
+      <!-- <h2 class="mb-4 py-2">hello, this is a test...</h2> -->
       <div>
         <v-select
           :items="myItems"
@@ -12,12 +12,6 @@
           solo
         >
         </v-select>
-        {{chosen}}
-        <!-- <div class="mb-4">
-          <div>
-            {{ chosen }}
-          </div>
-        </div> -->
       </div>
       <div class="levels-container">
         <div class="level">
@@ -28,14 +22,28 @@
           >
             <div
               @click="chosen.length ? join_it(index) : ''"
-              class="circle mb-3"
+              class="circle mb-7"
               :class="chosen.length > index ? 'active' : ''"
-            ></div>
+            >
+              <img
+                class="tick"
+                src="../assets/check-mark-svgrepo-com.svg"
+                alt=""
+              />
+            </div>
+            <div class="small-circle-wrapper mb-2">
+              <div
+                class="small-circle"
+                :class="chosen.length > index ? 'active' : ''"
+              ></div>
+            </div>
             <div class="text-center">
-              {{ chosen[index] }}
+              {{ levels[index] }}
             </div>
           </div>
         </div>
+
+        <!-- <div class="gray-line"></div> -->
       </div>
     </div>
   </div>
@@ -82,7 +90,6 @@ export default {
           // }
 
           this.$set(this.chosen, x, this.levels[x]);
-
         }
         for (let z = this.chosen.length; z < counter; z++) {
           this.$set(this.chosen, z, this.levels[z]);
@@ -94,10 +101,10 @@ export default {
       }
     },
   },
-  watch:{
-    chosen(newVal){
-      alert(newVal)
-    }
+  watch: {
+    chosen() {
+      // alert(newVal)
+    },
   },
   created() {
     this.getData();
@@ -105,29 +112,101 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.small-circle-wrapper {
+  position: relative;
+  &::after {
+    content: "";
+    width: 84%;
+    height: 2px;
+    background: gray;
+    position: absolute;
+    bottom: 9px;
+    left: -43%;
+  }
+}
+.small-circle {
+  width: 20px;
+  height: 20px;
+  margin: 0 auto;
+  border: solid 4px #adadad;
+  border-radius: 50%;
+  &.active {
+    border: solid 4px #00c59c;
+  }
+}
 .my-container {
   max-width: 800px;
   margin: 0 auto;
-  background: rgb(200, 248, 252);
   min-height: 100vh;
+}
+.levels-container {
+  box-shadow: 0 0 5px grey;
+  border-radius: 7px;
+  padding: 30px 0;
+  position: relative;
+  .gray-line {
+    position: absolute;
+    width: 86%;
+    height: 30px;
+    background: red;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 }
 .level {
   display: flex;
 }
 .circle-wrapper {
   width: calc(100% / 7);
-  // display: flex;
-  // justify-content: flex-start;
-
+  .tick {
+    display: none;
+  }
   .circle {
-    border: solid 2px rgb(81, 176, 3);
+    border: solid 3px #a7f8dd;
     height: 70px;
     width: 70px;
     border-radius: 50%;
-
+    position: relative;
+    margin: 0 auto;
     &.active {
-      background: green;
+      background: #e0fffc;
+      padding-left: 2px;
+      padding-top: 1px;
+      .tick {
+        display: block;
+      }
+    }
+    &.active ~ .small-circle-wrapper {
+      .small-circle {
+        background: #96ffd1;
+        // border-color: rgb(0, 170, 139);
+      }
+    }
+    &::after {
+      content: "";
+      width: 0;
+      height: 0;
+      border-left: 7px solid transparent;
+      border-right: 7px solid transparent;
+      border-top: 10px solid #a7f8dd;
+      position: absolute;
+      bottom: -11px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &:hover {
+      cursor: pointer;
     }
   }
+  &:last-child {
+    .small-circle-wrapper {
+      &::after {
+        display: none;
+      }
+    }
+  }
+}
+.rtl {
+  direction: rtl;
 }
 </style>
